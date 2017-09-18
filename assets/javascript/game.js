c0093d571dd7a3160b6caa9f52f58b1d92a0291b
Benjamin Lawson <lawbe101@yahoo.com>
2017-09-18 15:32:03 -0400

    changed name of creator to createGame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
 
+
+  // will be removed
   var crystalOptions = [1, 5, 7, 10];
 
+  // crystal numbers should be between 1-12
+  // will be removed, number should be randomly generated between 19-120
   function shuffle() {
     for (var i = 0; i < crystalOptions.length; i++) {
       var rand = Math.floor(Math.random() * (i + 1));
@@ -12,7 +16,7 @@ $(document).ready(function() {
     console.log('Test of shuffle', crystalOptions);
   }
 
-  function creator() {
+  function createGame() {
     for (var i = 0; i < crystalOptions.length; i++) {
       var images = $('<img>');
         images.attr('src', 'assets/images/crystal.png');
@@ -20,8 +24,10 @@ $(document).ready(function() {
         images.addClass('crystal-image');
       $('#crystals').append(images)
     }
+
+    var compNumber = Math.floor(Math.random() ())
   }
 
   shuffle();
-  creator();
+  createGame();
 })
