ec2dc0a844f49065e79b0be88f714f00f40908b0
Benjamin Lawson <lawbe101@yahoo.com>
2017-09-18 15:38:50 -0400

    removed array of numbers and switched to randomly generated. added computers number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,32 +2,36 @@ $(document).ready(function() {
 
 
   // will be removed
-  var crystalOptions = [1, 5, 7, 10];
+  // var crystalOptions = [1, 5, 7, 10];
 
-  // crystal numbers should be between 1-12
-  // will be removed, number should be randomly generated between 19-120
-  function shuffle() {
-    for (var i = 0; i < crystalOptions.length; i++) {
-      var rand = Math.floor(Math.random() * (i + 1));
-      var temp = crystalOptions[i];
-        crystalOptions[i] = crystalOptions[rand];
-        crystalOptions[rand] = temp;
-      }
-    console.log('Test of shuffle', crystalOptions);
-  }
+  // !!crystal numbers should be between 1-12
+  // !!number should be randomly generated between 19-120
+  // function shuffle() {
+  //   for (var i = 0; i < crystalOptions.length; i++) {
+  //     var rand = Math.floor(Math.random() * (i + 1));
+  //     var temp = crystalOptions[i];
+  //       crystalOptions[i] = crystalOptions[rand];
+  //       crystalOptions[rand] = temp;
+  //     }
+  //   console.log('Test of shuffle', crystalOptions);
+  // }
 
   function createGame() {
-    for (var i = 0; i < crystalOptions.length; i++) {
+    for (var i = 0; i < 4; i++) {
+      var crystalNumber = Math.floor(Math.random() * (19-1))+1;
       var images = $('<img>');
         images.attr('src', 'assets/images/crystal.png');
-        images.attr('value', crystalOptions[i]);
+        images.attr('value', crystalNumber);
         images.addClass('crystal-image');
       $('#crystals').append(images)
+      console.log(crystalNumber)
     }
 
-    var compNumber = Math.floor(Math.random() ())
+    var compNumber = Math.floor(Math.random() * (120-19)) + 19;
+    console.log(compNumber);
+
   }
 
-  shuffle();
+  // shuffle();
   createGame();
 })
