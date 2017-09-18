603d4d767a80abd0d539fa71735942f2b1fbce45
Benjamin Lawson <lawbe101@yahoo.com>
2017-09-18 12:20:23 -0400

    added function to shuffle the number options for the crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
-  console.log('I get here');
+  var crystalOptions = [1, 5, 7, 10];
 
+  function shuffle() {
+    for (var i = 0; i < crystalOptions.length; i++) {
+      var rand = Math.floor(Math.random() * (i + 1));
+      var temp = crystalOptions[i];
+        crystalOptions[i] = crystalOptions[rand];
+        crystalOptions[rand] = temp;
+      }
+    console.log('Test of shuffle', crystalOptions);
+  }
+
+  shuffle();
 })
