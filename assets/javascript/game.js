946aeef5cd64b6e5b768ef3f30648ada62508b9a
Benjamin Lawson <lawbe101@yahoo.com>
2017-09-18 16:31:30 -0400

    removed suffle function and compOptions array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,6 @@
 $(document).ready(function() {
-
-
-  // will be removed
-  // var crystalOptions = [1, 5, 7, 10];
-
   // !!crystal numbers should be between 1-12
   // !!number should be randomly generated between 19-120
-  // function shuffle() {
-  //   for (var i = 0; i < crystalOptions.length; i++) {
-  //     var rand = Math.floor(Math.random() * (i + 1));
-  //     var temp = crystalOptions[i];
-  //       crystalOptions[i] = crystalOptions[rand];
-  //       crystalOptions[rand] = temp;
-  //     }
-  //   console.log('Test of shuffle', crystalOptions);
-  // }
 
   function createGame() {
     for (var i = 0; i < 4; i++) {
@@ -24,11 +10,11 @@ $(document).ready(function() {
         images.attr('value', crystalNumber);
         images.addClass('crystal-image');
       $('#crystals').append(images)
-      console.log(crystalNumber)
+      // console.log(crystalNumber)
     }
 
     var compNumber = Math.floor(Math.random() * (120-19)) + 19;
-    console.log(compNumber);
+    // console.log(compNumber);
 
   }
 
